f4a2e35a19b98fcf8c93dc7de60903d9f76b4086
emanuel2401 <gonzalezemanuel0124@gmail.com>
2024-04-17 00:14:03 -0300

    match controller

---
+++ NetWebApi/Controllers/MatchController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model.Entities;
+
+namespace NetWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatchController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MatchController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMatches()
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET:
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMatch(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST: 
+        [HttpPost]
+        public async Task<IActionResult> CreateMatch(Match match)
+        {
+            throw new NotImplementedException();
+        }
+
+        // PUT: 
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateMatch(int id, Match match)
+        {
+            throw new NotImplementedException();
+        }
+
+        // DELETE: api/Match/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMatch(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repository/Repositories/MatchRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories
+{
+    internal class MatchRepository : Repository<Match>, IMatchRepository
+    {
+        public MatchRepository(DbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Match>> GetAllAsync()
+        {
+            return await Context.Set<Match>().Include(m => m.Club).ToListAsync();
+        }
+
+        public async Task<Match> GetByIdAsync(int id)
+        {
+            return await Context.Set<Match>().Include(m => m.Club).FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public bool Exists(int id)
+        {
+            return Context.Set<Match>().Any(e => e.Id == id);
+        }
+    {
+    }
+}
